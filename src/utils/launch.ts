24fab30457fae90be1981c2c92ae639bd9faaf4e
Dhino_Rahmad <dhinorahmad0@gmail.com>
2023-05-26 11:49:50 +0700

    webhook without costume session, solved in deploy

--- src/utils/launch.ts
+++ src/utils/launch.ts
@@ -1,8 +1,7 @@
-import { Bot, webhookCallback } from "grammy";
-import { MyContext } from "../core/bot";
+import { Bot, Context, webhookCallback } from "grammy"; 
 import express from "express";
 
-const production = async (bot: Bot<MyContext>): Promise<void> => {
+const production = async (bot: Bot<Context>): Promise<void> => {
     try {
         const app = express();
         app.use(express.json());
@@ -18,7 +17,7 @@ const production = async (bot: Bot<MyContext>): Promise<void> => {
     }
 };
 
-const development = async (bot: Bot<MyContext>): Promise<void> => {
+const development = async (bot: Bot<Context>): Promise<void> => {
     try {
         await bot.api.deleteWebhook();
         console.log("[SERVER] Bot starting polling");
